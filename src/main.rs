942a69a916e57e8e832a543682fc9666576a74c6
VernoxVernax <vernoxvernax@gmail.com>
2023-09-12 20:03:51 +0200

    faking user-agent and better timeouts; fixed local storage downloads

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@
 
 use std::{process::ExitCode, io::{self, Write, copy}, thread, time::Duration, fs::{self, File}};
 use clap::{Arg, Command, ArgAction};
-use isahc::{Request, RequestExt, ReadResponseExt};
+use isahc::{Request, RequestExt, ReadResponseExt, prelude::Configurable};
 use http::{StatusCode, Method};
+use rand::{Rng, rngs::ThreadRng};
 use serde_derive::Deserialize;
 use chrono::Utc;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+const USER_AGENT: &str = "Blink/2309051925 CFNetwork/1220.1 Darwin/20.3.0";
 
 #[derive(Debug, Clone)]
 struct Header {
@@ -74,6 +77,7 @@ struct SyncManifestInfo {
 
 #[derive(Debug, Deserialize)]
 struct SyncManifest {
+  manifest_id: String,
   clips: Vec<LocalClips>
 }
 
@@ -82,6 +86,13 @@ struct LocalClips {
   id: String,
   camera_name: String,
   created_at: String
+}
+
+#[derive(Debug, Deserialize)]
+struct CommandStatus {
+  complete: bool,
+  status: u8,
+  status_msg: String
 }
 
 fn main() -> ExitCode {
@@ -185,10 +196,9 @@ fn main() -> ExitCode {
         key: "Content-Type".to_string(),
         value: "application/json".to_string()
       };
-      let body: String = format!("{{\"unique_id\": \"00000000-0000-0000-0000-000000000000\", \"password\":\"{}\",\"email\":\"{}\", \"reauth\": \"true\"}}", password, email);
-
+      let body: String = format!("{{\"email\":\"{}\",\"password\":\"{}\",\"unique_id\":\"{}\",\"device_identifier\":\"BlinkApp\",\"client_name\":\"Computer\",\"reauth\":\"true\"}}", email, password, gen_uid(16, true));
       loop {
-        if let Ok(res) = blink_post(&format!("https://{}/api/v5/account/login", global_domain), header.clone(), None, body.clone()) {
+        if let Ok(res) = blink_post(&format!("https://{}/api/v5/account/login", global_domain), header.clone(), None, Some(body.clone())) {
           let json_res = serde_json::from_str::<Login>(&res).unwrap();
           let region_domain = if cli.get_one::<String>("domain").is_some() {
             cli.get_one::<String>("domain").unwrap().to_string()
@@ -204,27 +214,28 @@ fn main() -> ExitCode {
             print!("Please enter the pin for the device-verification.\n: ");
             let pin = get_input();
 
-
             let url = format!("https://{}/api/v4/account/{}/client/{}/pin/verify", region_domain, json_res.account.account_id, json_res.account.client_id);
+
             let header: Header = Header {
               key: "Content-Type".to_string(),
               value: "application/json".to_string()
             };
+
             let auth_header: Header = Header {
               key: "TOKEN-AUTH".to_string(),
               value: json_res.auth.token.clone()
             };
 
-            let verify_request = blink_post(&url, header, Some(auth_header.clone()), format!("{{\"pin\": {} }}", pin));
+            let verify_request = blink_post(&url, header, Some(auth_header.clone()), Some(format!("{{\"pin\": {} }}", pin)));
 
             if verify_request.is_err() {
               println!("Invalid pin provided. Please try again ...");
             } else {
               println!("Success");
-              blink_sync(&region_domain, json_res, auth_header, *wait, since, download_folder, download_cloud_media, download_local_media);
+              blink_sync(&global_domain, &region_domain, json_res, auth_header, *wait, since, download_folder, download_cloud_media, download_local_media);
             }
           } else {
-            blink_sync(&region_domain, json_res, auth_header, *wait, since, download_folder, download_cloud_media, download_local_media);
+            blink_sync(&global_domain, &region_domain, json_res, auth_header, *wait, since, download_folder, download_cloud_media, download_local_media);
             thread::sleep(Duration::from_secs(*wait as u64));
           }
         } else {
@@ -239,7 +250,34 @@ fn main() -> ExitCode {
   ExitCode::SUCCESS
 }
 
-fn blink_sync(regional_domain: &String, session: Login, auth_header: Header, wait: u8, since: u64, download_folder: &str,
+fn gen_uid(size: usize, uid_format: bool) -> String {
+  let mut rng = rand::thread_rng();
+
+  if uid_format {
+    let uid = format!(
+      "BlinkCCamera_{}-{}-{}-{}-{}",
+      gen_random_hex(4, &mut rng),
+      gen_random_hex(2, &mut rng),
+      gen_random_hex(2, &mut rng),
+      gen_random_hex(2, &mut rng),
+      gen_random_hex(6, &mut rng)
+    );
+    return uid;
+  } else {
+    return gen_random_hex(size, &mut rng);
+  }
+}
+
+fn gen_random_hex(size: usize, rng: &mut ThreadRng) -> String {
+  let mut hex_string = String::new();
+  for _ in 0..size {
+    let digit: u8 = rng.gen_range(0..16);
+    hex_string.push_str(&format!("{:x}", digit));
+  }
+  hex_string
+}
+
+fn blink_sync(global_domain: &String, regional_domain: &String, session: Login, auth_header: Header, wait: u8, since: u64, download_folder: &str,
   download_cloud_media: bool, download_local_media: bool) {
   loop {
     let current_time = Utc::now();
@@ -319,7 +357,7 @@ fn blink_sync(regional_domain: &String, session: Login, auth_header: Header, wai
             let url_manifest_id = format!("https://{}/api/v1/accounts/{}/networks/{}/sync_modules/{}/local_storage/manifest/request",
             regional_domain, session.account.account_id, sync_module.network_id, sync_module.id);
 
-            let manifest_info = if let Ok(res) = blink_post(&url_manifest_id, auth_header.clone(), None, String::from("")) {
+            let manifest_info = if let Ok(res) = blink_post(&url_manifest_id, auth_header.clone(), None, None) {
               serde_json::from_str::<SyncManifestInfo>(&res).unwrap()
             } else {
               continue;
@@ -333,13 +371,14 @@ fn blink_sync(regional_domain: &String, session: Login, auth_header: Header, wai
                 full_manifest = serde_json::from_str::<SyncManifest>(&res).unwrap();
                 break;
               }
-              thread::sleep(Duration::from_secs(10 as u64));  
+              thread::sleep(Duration::from_secs(5 as u64));  
             }
 
             for video in full_manifest.clips {
               let output = format!("./{}/{}_{}_{}.mp4",
               download_folder, network_name, video.camera_name, video.created_at.replace(":", "-"));
               
+              fs::create_dir_all(format!("./{}", download_folder)).unwrap();
               if fs::metadata(output.clone()).is_ok() {
                 continue;
               } else {
@@ -348,22 +387,30 @@ fn blink_sync(regional_domain: &String, session: Login, auth_header: Header, wai
               
               // The clip has to be uploaded to blink's servers first to download it.
               let url_clip = format!("https://{}/api/v1/accounts/{}/networks/{}/sync_modules/{}/local_storage/manifest/{}/clip/request/{}",
-              regional_domain, session.account.account_id, sync_module.network_id, sync_module.id, manifest_info.id, video.id);
-              
-              if let Err(()) = blink_post(&url_clip, auth_header.clone(), None, String::from("")) {
-                println!("Upload failed. Continuing in 10 seconds ...");
-                thread::sleep(Duration::from_secs(10 as u64));
-                continue;
-              };
-
-              thread::sleep(Duration::from_secs(5 as u64));
-              fs::create_dir_all(format!("./{}", download_folder)).unwrap();
-
-              if download_video(&url_clip, auth_header.clone(), &output).is_ok() {
-                thread::sleep(Duration::from_secs(5 as u64));
-              } else {
-                println!("Download Failed. Trying next clip ...");
-                thread::sleep(Duration::from_secs(5 as u64));
+              regional_domain, session.account.account_id, sync_module.network_id, sync_module.id, full_manifest.manifest_id, video.id);
+              'upload: for _ in 1..4 {
+                if let Ok(res) = blink_post(&url_clip, auth_header.clone(), None, None) {
+                  thread::sleep(Duration::from_secs(2 as u64));
+                  let upload_id = serde_json::from_str::<SyncManifestInfo>(&res).unwrap().id;
+                  let url_upload_state = format!("https://{}/network/{}/command/{}", regional_domain, sync_module.network_id, upload_id);
+                  for _ in 0..6 {
+                    if let Ok(res) = blink_get(&url_upload_state, auth_header.clone()) {
+                      let command_status = serde_json::from_str::<CommandStatus>(&res).unwrap();
+                      if command_status.complete {
+                        if download_video(&url_clip, auth_header.clone(), &output).is_err() {
+                          println!("Download Failed. Trying next clip ...");
+                        }
+                        thread::sleep(Duration::from_secs(2 as u64));
+                        break 'upload;
+                      }
+                    }
+                    thread::sleep(Duration::from_secs(2 as u64));
+                  }
+                } else {
+                  println!("Upload failed. Continuing in 10 seconds ...");
+                  thread::sleep(Duration::from_secs(10 as u64));
+                  continue;
+                };
               }
             }
           }
@@ -390,6 +437,9 @@ fn download_video(url: &String, auth_header: Header, output: &String) -> Result<
     .method(Method::GET)
     .uri(url)
     .header(auth_header.key, auth_header.value)
+    .header("User-Agent", USER_AGENT)
+    .header("content-type", "application/json")
+    .timeout(Duration::from_secs(20))
     .body(()).unwrap()
   .send();
 
@@ -398,10 +448,6 @@ fn download_video(url: &String, auth_header: Header, output: &String) -> Result<
   }
 
   let res = request.unwrap();
-  if res.status() != StatusCode::OK {
-    // println!("{:?}", res.text().unwrap());
-    return Err(());
-  }
 
   println!("Saving: {:?}", output);
 
@@ -417,6 +463,9 @@ fn blink_get(url: &String, header: Header) -> Result<String, ()> {
   let request = Request::get(url)
     .method(Method::GET)
     .header(header.key, header.value)
+    .header("content-type", "application/json")
+    .header("User-Agent", USER_AGENT)
+    .timeout(Duration::from_secs(20))
     .body(()).unwrap()
   .send();
 
@@ -440,18 +489,21 @@ fn blink_get(url: &String, header: Header) -> Result<String, ()> {
   }
 }
 
-fn blink_post(url: &String, header: Header, header2: Option<Header>, body: String) -> Result<String, ()> {
+fn blink_post(url: &String, header: Header, header2: Option<Header>, body: Option<String>) -> Result<String, ()> {
   let mut builder = Request::builder()
     .uri(url)
     .method(Method::POST)
-  .header(header.key, header.value);
+    .header(header.key, header.value)
+    .header("User-Agent", USER_AGENT)
+    .header("content-type", "application/json")
+  .timeout(Duration::from_secs(20));
 
   if let Some(header) = header2 {
     builder = builder.header(header.key, header.value);
   }
 
-  let request = if ! body.is_empty() {
-    builder.body(body).unwrap().send()
+  let request = if body.is_some() {
+    builder.body(body.unwrap()).unwrap().send()
   } else {
     builder.body(()).unwrap().send()
   };
